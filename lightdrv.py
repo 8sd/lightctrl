5ad08b29a2cf09203c2c23c5b5b5b9a8e1eb0b97
dudas <sebastian.duda@fau.de>
2018-12-21 21:02:26 +0100

    implement fixes

--- lightdrv.py
+++ lightdrv.py
@@ -5,15 +5,15 @@ class Lightdrv(object):
     val = [0.0, 0.0, 0.0, 0.0]
     dev = Device(0x40)
 
-    def __init__ (self):
-        self.dev.set_pwm_frequency(1000)
-
     def set(self, vec):
         for i in range(0, len(vec)):
-            if vec[i] < 0 | vec[i] > 4095:
+            if (vec[i] < 0) | (vec[i] > 4095):
                 continue
-            self.dev.set_pwm(i, int(vec[i]))
-        self.val = vec
+	    self.val = vec
+            try:
+                self.dev.set_pwm(i, int(vec[i]))
+            except Exception:
+                pass
 
     def get(self):
         return self.val
