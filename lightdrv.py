9353dcc32eed768dc3a2d87bd7e4d61f7984186c
Sebastian Duda <github@sebdu.de>
2018-12-21 18:02:25 +0000

    Implement pca lib

--- lightdrv.py
+++ lightdrv.py
@@ -1,8 +1,18 @@
+from pca9685_driver import Device
+
+
 class Lightdrv(object):
     val = [0.0, 0.0, 0.0, 0.0]
+    dev = Device(0x40)
+
+    def __init__ (self):
+        self.dev.set_pwm_frequency(1000)
 
     def set(self, vec):
-        print(vec)
+        for i in range(0, len(vec)):
+            if vec[i] < 0 | vec[i] > 4095:
+                continue
+            self.dev.set_pwm(i, vec[i])
         self.val = vec
 
     def get(self):
